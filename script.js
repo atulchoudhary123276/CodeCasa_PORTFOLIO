3ea9b1b4f1b831b2f330f6d224cafb1299cb01b5
Atul Kumar <133472429+atulchoudhary123276@users.noreply.github.com>
2023-09-11 20:09:17 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+
+// <!-- Setup and start animation! -->
+
+    let typed = new Typed('#element', {
+        strings: ['Web Developer', 'Graphic Designer', 'JS Developer'],
+        typeSpeed: 50,
+    });
+   
+    let btn=document.getElementById('btn');
+    function clickFunc(){
+        // window.location="https://github.com/atulchoudhary123276/CodeCasa_PORTFOLIO.git";
+        window.open('https://github.com/atulchoudhary123276/CodeCasa_PORTFOLIO.git');    // for new tab
+    }
